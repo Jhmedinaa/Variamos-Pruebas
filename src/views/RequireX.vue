f7c32f9bf04d4cc9e3b1bda6a8dcc4f28b81de7a
jhon <jhoham@hotmail.es>
2019-03-13 11:30:39 -0500

    SE realizan validación del formulario, se inicia concatenación.

--- src/views/RequireX.vue
+++ src/views/RequireX.vue
@@ -1,247 +1,297 @@
 <template>
-<v-form v-model="formValid">
-    <v-jsonschema-form v-if="schema" :schema="schema" :model="dataObject" :options="options" @error="showError" />
-  </v-form>
+  <div>
+    <div v-if="validation">
+      <v-alert color="red" :value="!formValid" type="error" transition="scale-transition">Please, check the fields.</v-alert>
+    </div>
+    <div>
+      <v-form v-model="formValid" ref="requireXForm">
+        <v-jsonschema-form
+          v-if="schema"
+          :schema="schema"
+          :model="dataObject"
+          :options="options"
+          @error="showError"
+        />
+        <v-btn @click="submit()" color="dark">Generate</v-btn>
+      </v-form>
+      
+    </div>
+
+    <div class="text-xs-center">
+      <v-dialog v-model="dialog" width="500">
+    
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>RequireX</v-card-title>
+
+          <v-card-text>
+            <pre>{{ JSON.stringify(dataObject, null, 2) }}</pre>
+            <p>{{this.require}}</p>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click="dialog = false">I accept</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import Draggable from 'vuedraggable'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import Swatches from 'vue-swatches'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import Draggable from "vuedraggable";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import Swatches from "vue-swatches";
 
-import VJsonschemaForm from '@koumoul/vuetify-jsonschema-form'
+import VJsonschemaForm from "@koumoul/vuetify-jsonschema-form";
 
-Vue.use(Vuetify)
-Vue.use(VueAxios, axios)
+Vue.use(Vuetify);
+Vue.use(VueAxios, axios);
 
-Vue.component('swatches', Swatches)
-Vue.component('draggable', Draggable)
+Vue.component("swatches", Swatches);
+Vue.component("draggable", Draggable);
 
 export default {
-  components: {VJsonschemaForm},
+  components: { VJsonschemaForm },
   data() {
-    return {
+    return {      
+      dialog: false,
+      require: "",
       schema: {
-  "description": "RequireX Template",
-  "type": "object",
- "properties": {
-   "ReqType":{
-      "title":"Requirement Type",
-      "type":"string",
-      "oneOf":[
-        {
-          "const":"Constraint",
-          "title":"Constraint"
-        },
-        {
-          "const":"FunctionalReq",
-          "title":"Functional Requirements"
-        },
-                {
-          "const":"QualityReq",
-          "title":"Quality Requirement"
-        }
-      ]
-    },
-    "name": {
-      "title":"Requirements name",
-      "type": "string"
-    },
-  },
-  "allOf": [
-    {
-      "$ref": "#/definitions/propertiesA"
-    },
-    {
-      "$ref": "#/definitions/propertiesB"
-    }
-  ],
-  "oneOf": [
-    {
-      "$ref": "#/definitions/userInt"
-    },
-    {
-      "$ref": "#/definitions/autoAct"
-    },
-    {
-      "$ref": "#/definitions/extInt"
-    }
-  ],
-  "definitions": {
-    "propertiesA": {
-      "properties": {
-        "condition": {
-        "title": "Requirement have a conditional?",
-        "type": "boolean"
-        }  
-      },
-      "dependencies": {
-        "condition": {
-          "properties": {
-            "ConditionDescription": {
-              "title":"Plase enter a condition",
-              "type": "string",
-              "maxLength": 2000
-            }
+        description: "RequireX Template",
+        type: "object",
+        required: ["reqType", "name", "imperative"],
+        properties: {
+          reqType: {
+            title: "Requirement Type",
+            type: "string",
+            oneOf: [
+              {
+                const: "Constraint",
+                title: "Constraint"
+              },
+              {
+                const: "FunctionalReq",
+                title: "Functional Requirements"
+              },
+              {
+                const: "QualityReq",
+                title: "Quality Requirement"
+              }
+            ]
+          },
+          name: {
+            title: "Requirements name",
+            type: "string"
+          },
+          condition: {
+            title: "Requirement have a conditional?",
+            type: "boolean"
+          },
+          imperative: {
+            title: "Imperative",
+            type: "string",
+            enum: ["Must", "Should", "Could"]
+          },
+          systemActivity: {
+            title: "System Activity",
+            oneOf: [
+              {
+                $ref: "#/definitions/userInt"
+              },
+              {
+                $ref: "#/definitions/autoAct"
+              },
+              {
+                $ref: "#/definitions/extInt"
+              }
+            ]
           }
-        }
-      }
-    },
-    "propertiesB": {
-      "properties": {
-        "Imperative":{
-          "title":"Imperative",
-          "type":"string",
-          "enum":[
-            "Must", "Should","Could"
-          ]
         },
-      },
-        "dependencies":{
-          "Imperative":{
-            "properties":{
-              "SystemName":{
-                "title":"System or System Family Name",
-                "type":"string"
+        dependencies: {
+          condition: {
+            properties: {
+              conditionDescription: {
+                title: "Plase enter a condition",
+                type: "string",
+                maxLength: 2000
+              }
+            },
+            required: ["conditionDescription"]
+          },
+          imperative: {
+            properties: {
+              systemName: {
+                title: "System or System Family Name",
+                type: "string"
+              }
+            },
+            required: ["systemName"]
+          }
+        },
+        oneOf: [
+          {
+            $ref: "#/definitions/userInt"
+          },
+          {
+            $ref: "#/definitions/autoAct"
+          },
+          {
+            $ref: "#/definitions/extInt"
+          }
+        ],
+        definitions: {
+          userInt: {
+            title: "User Interface",
+            properties: {
+              type: {
+                type: "string",
+                title: "System Activity",
+                const: "userInt"
+              },
+              user: {
+                title: "User",
+                type: "string"
+              },
+              processVerb: {
+                title: "Process Verb",
+                type: "string"
+              },
+              object: {
+                title: "Objet (Noun)",
+                type: "string"
+              },
+              objectCondition: {
+                title: "Object have a conditional?",
+                type: "boolean"
+              }
+            },
+            required: ["user", "processVerb", "object"],
+            dependencies: {
+              objectCondition: {
+                properties: {
+                  conditionDescription: {
+                    type: "string",
+                    title:
+                      'Plase enter a condition of the type "if and only if"',
+                    maxLength: 2000
+                  }
+                },
+                required: ["conditionDescription"]
               }
             }
-            
-          }
-        }
-    },
-    "userInt": {
-      "title": "User Interface",
-      "properties": {
-        "type": {
-          "type": "string",
-          "title": "System Activity",
-          "const": "userInt"
-        },
-        "User":{
-          "title":"User",
-          "type":"string"
-        },
-        "ProcessVerb":{
-          "title":"Process Verb",
-          "type":"string"
-        },
-        "Object":{
-          "title":"Objet (Noun)",
-          "type":"string"
-        },
-        "Objectcondition": {
-        "title": "Object have a conditional?",
-        "type": "boolean"
-        }  
-      },
-      "dependencies":{
-        "Objectcondition": {
-          "properties": {
-            "ConditionDescription": {
-              "type": "string",
-              "title":"Plase enter a condition of the type \"if and only if\"",
-              "maxLength": 2000
+          },
+          autoAct: {
+            title: "Autonomous Activity",
+            properties: {
+              type: {
+                type: "string",
+                const: "autoAct"
+              },
+              processVerb: {
+                title: "Process Verb",
+                type: "string"
+              },
+              object: {
+                title: "Objet (Noun)",
+                type: "string"
+              },
+              objectCondition: {
+                title: "Object have a conditional?",
+                type: "boolean"
+              }
+            },
+            required: ["processVerb", "object"],
+            dependencies: {
+              objectCondition: {
+                properties: {
+                  conditionDescription: {
+                    type: "string",
+                    title:
+                      'Plase enter a condition of the type "if and only if"',
+                    maxLength: 2000
+                  }
+                },
+                required: ["conditionDescription"]
+              }
+            }
+          },
+          extInt: {
+            title: "External Interface",
+            properties: {
+              type: {
+                type: "string",
+                const: "extInt"
+              },
+              system: {
+                title: "External System or Device",
+                type: "string"
+              },
+              from: {
+                title: "Interface requirement interaction",
+                type: "string",
+                enum: ["From", "To"]
+              },
+              processVerb: {
+                title: "Process Verb",
+                type: "string"
+              },
+              object: {
+                title: "Objet (Noun)",
+                type: "string"
+              },
+              objectCondition: {
+                title: "Object have a conditional?",
+                type: "boolean"
+              }
+            },
+            required: ["system", "from", "processVerb", "object"],
+            dependencies: {
+              objectCondition: {
+                properties: {
+                  conditionDescription: {
+                    type: "string",
+                    title:
+                      'Plase enter a condition of the type "if and only if"',
+                    maxLength: 2000
+                  }
+                },
+                required: ["conditionDescription"]
+              }
             }
           }
         }
-      }
-    },
-    "autoAct": {
-      "title": "Autonomous Activity",
-      "properties": {
-        "type": {
-          "type": "string",
-          "const": "autoAct"
-        },
-        "ProcessVerb":{
-          "title":"Process Verb",
-          "type":"string"
-        },
-        "Object":{
-          "title":"Objet (Noun)",
-          "type":"string"
-        },
-        "Objectcondition": {
-        "title": "Object have a conditional?",
-        "type": "boolean"
-        }  
       },
-      "dependencies":{
-        "Objectcondition": {
-          "properties": {
-            "ConditionDescription": {
-              "type": "string",
-              "title":"Plase enter a condition of the type \"if and only if\"",
-              "maxLength": 2000
-            }
-          }
-        }
-      }
-    },
-    "extInt": {
-      "title":"External Interface",
-      "properties": {
-        "type":{
-          "type": "string",
-          "const": "extInt"
-        },
-        "System":{
-          "title":"External System or Device",
-          "type":"string"
-        },
-        "From":{
-        "title": "Interface requirement interaction",
-        "type": "string",
-        "enum":[
-          "From","To"
-        ]
-        },
-        "ProcessVerb":{
-          "title":"Process Verb",
-          "type":"string"
-        },
-        "Object":{
-          "title":"Objet (Noun)",
-          "type":"string"
-        },
-        "Objectcondition": {
-        "title": "Object have a conditional?",
-        "type": "boolean"
-        } 
-      },
-      "dependencies":{
-        "Objectcondition": {
-          "properties": {
-            "ConditionDescription": {
-              "type": "string",
-              "title":"Plase enter a condition of the type \"if and only if\"",
-              "maxLength": 2000
-            }
-          }
-        }
-      }
-    }
-  }
-  
-},
       dataObject: {},
       formValid: false,
+      validation: false,
       options: {
         debug: false,
         disableAll: false,
         autoFoldObjects: true
       }
-    }
+    };
   },
   methods: {
     showError(err) {
-      window.alert(err)
+      window.alert(err);
+    },
+    submit() {
+      if (this.$refs.requireXForm.validate()) {   
+        //Ejemplo de concatenación
+        this.require = "Example concatenation" + this.dataObject.name + " " + this.dataObject.reqType;
+        this.dialog = true;
+      } else {
+        this.validation = true;
+      }
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
